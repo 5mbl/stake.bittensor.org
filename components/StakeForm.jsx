1b0000924bb348301e32fa02630cb407ce40d00b
Serdar Palaoglu <nyrox.plg@gmail.com>
2024-02-12 21:56:27 +0100

    update staking form

--- components/StakeForm.jsx
+++ components/StakeForm.jsx
@@ -2,7 +2,7 @@
 import React, { useState } from "react";
 import { web3FromSource } from "@polkadot/extension-dapp";
 
-const StakeForm = ({ api, selectedValidator, accountAddress }) => {
+const StakeForm = ({ api, selectedValidator, accountAddress, goBack }) => {
   const [stakeAmount, setStakeAmount] = useState("");
   const [stakingStatus, setStakingStatus] = useState("");
 
@@ -55,21 +55,28 @@ const StakeForm = ({ api, selectedValidator, accountAddress }) => {
   };
 
   return (
-    <div>
+    <div className="text-gray-900">
       <input
         type="number"
         value={stakeAmount}
         onChange={handleStakeAmountChange}
         placeholder="Amount to Stake"
-        className="mb-4 p-2 border rounded"
+        className="mb-4 p-2 border border-gray-300 rounded shadow-sm focus:border-black"
       />
       <button
         onClick={handleStake}
-        className="bg-green-500 text-white px-4 py-2 rounded-md"
+        className="bg-gray-800 ml-2 text-white px-4 py-2 rounded-md transition duration-300 ease-in-out hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-gray-700 focus:ring-opacity-50"
       >
         Submit Stake
       </button>
-      {stakingStatus && <p>{stakingStatus}</p>}
+      {stakingStatus && <p className="text-sm mt-2">{stakingStatus}</p>}
+      {/* ... form inputs and submit button */}
+      <button
+        onClick={goBack}
+        className="bg-gray-300 text-black px-4 py-2 rounded-md text-md mt-2"
+      >
+        Go Back
+      </button>
     </div>
   );
 };
